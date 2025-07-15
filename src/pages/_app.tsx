d14b7a36dfc9c63ddcef3dbc89df2f0e0fe16dc8
scoful <1269717999@qq.com>
2025-07-15 11:03:51 +0800

    chore: 移除不必要的Geist字体导入，简化_app.tsx组件结构；更新全局样式以使用浏览器默认字体，提升样式一致性

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Geist } from "next/font/google";
 
 import { api } from "@/utils/api";
 import { RefreshProvider } from "@/contexts/RefreshContext";
@@ -9,10 +8,6 @@ import { NotificationProvider } from "@/components/Layout/NotificationProvider";
 import { VersionProvider } from "@/contexts/VersionContext";
 
 import "@/styles/globals.css";
-
-const geist = Geist({
-  subsets: ["latin"],
-});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -23,9 +18,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <VersionProvider>
         <RefreshProvider>
           <NotificationProvider position="top-center">
-            <div className={geist.className}>
-              <Component {...pageProps} />
-            </div>
+            <Component {...pageProps} />
           </NotificationProvider>
         </RefreshProvider>
       </VersionProvider>
